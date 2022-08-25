8ba20a0d989224448107af9408b71699a19203d9
EdgyTyler <112088967+EdgyTyler@users.noreply.github.com>
2022-08-25 14:31:50 +0200

    Added StarWars Buddies

---
+++ StarWarsBuddies/Program.cs
@@ -0,0 +1,72 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        Root r = JsonConvert.DeserializeObject<Root>(GetSomething("https://swapi.dev/api/people").Result);
+        List<string> Buddies = new List<string>();
+        Dictionary<string, string> dictBuddies = new Dictionary<string, string>();
+
+        foreach (Result ResultOb in r.results)
+
+        {
+            string Filmsss = string.Join(",", ResultOb.films.ToArray());
+
+            if (dictBuddies.ContainsKey(Filmsss))
+            {
+                dictBuddies[Filmsss] = dictBuddies[Filmsss] + " , " + ResultOb.name;
+            }
+            else
+            {
+             
+                dictBuddies.Add(Filmsss, ResultOb.name);
+            }
+
+        }
+
+        foreach (KeyValuePair<string, string> b in dictBuddies)
+        {
+
+            if (b.Value.Split(",").Length > 1)
+            {
+                Console.WriteLine(b.Value);
+            }
+
+        }
+
+
+    }
+
+
+    static async Task<string>  GetSomething(string url)
+    {
+
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(url);
+   
+        return  await response.Content.ReadAsStringAsync(); ;
+    }
+
+    public class Result
+    {
+        public string name;
+        public List<string> films;
+    }
+
+    public class Root
+    {
+        public object next;
+        public List<Result> results;
+    }
+
+
+
+}
+
